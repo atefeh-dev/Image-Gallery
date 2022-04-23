1fd6235ae8e115141e1df1dd762b943df6ecd56c
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-04-23 12:19:19 +0430

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 /** @format */
 import "../style/Header.scss";
+import React, { useRef, useState } from "react";
+
 const Header = () => {
+  const [isFloatingInputActive, setFloatingInputActive] = useState(false);
+  const inputEl = useRef();
+  const floatingInputEl = useRef();
+
   return (
     <header>
       <button className="btn home">
@@ -28,6 +34,65 @@ const Header = () => {
         </div>
         <span className="title">Gallery</span>
       </button>
+      <form>
+        <div className="form-group g-1">
+          <input type="text" placeholder="Type here" ref={inputEl} />
+          <button
+            className="btn search"
+            disabled={
+              inputEl.current !== undefined &&
+              inputEl.current.value.length === 0
+                ? true
+                : false
+            }>
+            <div className="icon search">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </div>
+            <span>search</span>
+          </button>
+        </div>
+        <div className="form-group g-2">
+          <button className="btn search">
+            <div className="icon search">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </div>
+          </button>
+        </div>
+
+        <div
+          className={`floating--container ${
+            isFloatingInputActive ? "" : "hide"
+          }`}>
+          <div className="input--container">
+            <input type="text" placeholder="Type here" ref={floatingInputEl} />
+            <button
+              className="btn search"
+              disabled={
+                floatingInputEl.current !== undefined &&
+                floatingInputEl.current.value.length === 0
+                  ? true
+                  : false
+              }>
+              search
+            </button>
+          </div>
+
+          <button className="btn close">
+            <div className="icon cross">
+              <svg
+                viewBox="0 0 512 512"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="false">
+                <path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z" />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </form>
     </header>
   );
 };
