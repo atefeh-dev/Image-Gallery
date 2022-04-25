a891b55602a95869d05d9c5c58c61029663813e4
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-04-25 18:39:43 +0430

    fix disable button when input is empty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 /** @format */
 import "../style/Header.scss";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const Header = () => {
   const [isFloatingInputActive, setFloatingInputActive] = useState(false);
+  const [name, setName] = useState();
 
   const inputEl = useRef();
 
@@ -13,7 +14,9 @@ const Header = () => {
   const handleHomePage = () => {
     inputEl.current.value = "";
   };
-
+  useEffect(() => {
+    console.log(inputEl.current.value);
+  });
   return (
     <header>
       <button className="btn home" onClick={handleHomePage}>
@@ -44,15 +47,13 @@ const Header = () => {
 
       <form onSubmit={handleSubmit}>
         <div className="form-group g-1">
-          <input type="text" placeholder="Type here" ref={inputEl} />
-          <button
-            className="btn search"
-            disabled={
-              inputEl.current !== undefined &&
-              inputEl.current.value.length === 0
-                ? true
-                : false
-            }>
+          <input
+            type="text"
+            placeholder="Type here"
+            ref={inputEl}
+            onChange={() => setName(inputEl.current.value)}
+          />
+          <button className="btn search" disabled={!name}>
             <div className="icon search">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
