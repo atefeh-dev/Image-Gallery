9565e404843535de5e43256c931450cb6d4d9169
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-04-28 15:27:26 +0430

    get character from gql and show in list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -18,7 +18,20 @@ const GET_CHARACTERS = gql`
 const ImageList = () => {
   const { error, data, loading } = useQuery(GET_CHARACTERS);
   console.log({ error, data, loading });
-  return <div>hi list</div>;
+  if (loading) return <div>spinner...</div>;
+  if (error) return <div>Something went wrong</div>;
+  return (
+    <div>
+      {data.characters.results.map((character) => {
+        return (
+          <div>
+            <img src={character.image} />
+            <h2>{character.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ImageList;
