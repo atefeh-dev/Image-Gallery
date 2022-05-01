8af7f0ad2eb41fd1bcc59f20cd0806fdb1b137f0
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-01 17:46:49 +0430

    -a

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -2,6 +2,22 @@
 
 import "../style/NotFoundPage.scss";
 const NotFound = () => {
-  return <div>404 not found</div>;
+  return (
+    <div className="s-page">
+      <h3 className="titlen">OH JEEZ, IT'S A</h3>
+      <h2 className="four1">4</h2>
+      <h2 className="four">4</h2>
+      <div className="morty">
+        <div className="hair"></div>
+        <div className="ears"></div>
+        <div className="face"></div>
+        <div className="eyes"></div>
+        <div className="nose"></div>
+        <div className="mouth"></div>
+        <div className="lines"></div>
+      </div>
+      <h1 className="title2">ERROR</h1>
+    </div>
+  );
 };
 export default NotFound;
