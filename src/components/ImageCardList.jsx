c19911209f20e083f3e57bffcf481354284e8fbf
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-03 10:40:37 +0430

    -a

--- src/components/ImageCardList.jsx
+++ src/components/ImageCardList.jsx
@@ -6,12 +6,19 @@ import { Link } from "react-router-dom";
 const ImageCardList = (data) => {
   console.log(data.data.name);
   return (
-    <div>
-      <Link to={`/${data.data.id}`}>
-        <img src={data.data.image} />
-        <h2>{data.data.name}</h2>
-      </Link>
-    </div>
+    <Link to={`/${data.data.id}`}>
+      <div className="masonry-brick">
+        <div className="image-card masonry-content">
+          <img className="image" src={data.data.image} />
+          <div className="image-card--clickable-area" onClick={() => {}} />
+          <div className="image-card--options">
+            <div className="user-info">
+              <h3>{data.data.name}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 };
 export default ImageCardList;
